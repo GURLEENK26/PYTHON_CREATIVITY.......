69d9c194fc256e7b32eb388c8a51e8596c7ac07e
GURLEENK26 <93379248+GURLEENK26@users.noreply.github.com>
2021-11-10 17:42:37 +0530

    Create YourName.py

---
+++ YourName.py
@@ -0,0 +1,7 @@
+import turtle
+A=input("Enter Your Name: ")
+turtle.bgcolor('black')
+turtle.color('Red')
+style = ('Courier', 30, 'bold', 'italic')
+turtle.write(A, font=style, align='center')
+turtle.hideturtle()
