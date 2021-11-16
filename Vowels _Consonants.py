870e8e7ad6eba8d1f4f44055648d2f6808c51e0d
GURLEEN KAUR <93379248+GURLEENK26@users.noreply.github.com>
2021-11-16 22:43:12 +0530

    Create Vowels _Consonants.py

---
+++ Vowels _Consonants.py
@@ -0,0 +1,12 @@
+#IN_CASE_OF_SENTENCE
+G = input('Enter Any Sentence: ')
+
+vowels = [each for each in G if each in "aeiouAEIOU"]
+
+consonants = [each for each in G if each not in "aeiouAEIOU "]
+
+print('Number of vowel in string:', len(vowels))
+print(vowels)
+
+print('Number of consonant in string:', len(consonants))
+print(consonants)
